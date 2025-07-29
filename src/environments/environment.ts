220478674e60435621d512723b2026b8cc35d03e
deuvede24 <cdaniela.vd@gmail.com>
2025-07-30 00:55:41 +0200

    chore: initial project scaffold & install dependencies

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiZGFuaXZpbGxhZGlheiIsImEiOiJjbWRrNXNiOHQwdTEzMmtzYzllMDAzdTZpIn0.KzUjQbqLneDRQBFsgx-k3Q'
+};
