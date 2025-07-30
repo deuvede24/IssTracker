d2e854f229b4e1e68367744fc16d4b5e9036da0f
deuvede24 <cdaniela.vd@gmail.com>
2025-07-30 12:43:19 +0200

    🔒 Remove environment.ts from tracking, update .gitignore and clean environment.prod.ts

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  mapboxToken: 'pk.eyJ1IjoiZGFuaXZpbGxhZGlheiIsImEiOiJjbWRrNXNiOHQwdTEzMmtzYzllMDAzdTZpIn0.KzUjQbqLneDRQBFsgx-k3Q'
-};
