e2e57af97b01139e0ea5bb3eb2d8447d62530d36
deuvede24 <cdaniela.vd@gmail.com>
2025-07-31 20:22:15 +0200

    refactor(interfaces): add optional direction, compass and azimuth fields to PassHome

--- src/app/interfaces/pass.interface.ts
+++ src/app/interfaces/pass.interface.ts
@@ -1,20 +1,23 @@
 export interface PassCore {
-  readonly id: string;
-  readonly time: Date;
-  readonly duration: number;
-  readonly from: string;
-  readonly to: string;
+    readonly id: string;
+    readonly time: Date;
+    readonly duration: number;
+    readonly from: string;
+    readonly to: string;
 }
 
 export interface PassMap extends PassCore {
-  readonly timeToPass?: string;
+    readonly timeToPass?: string;
 }
 
 export interface PassHome extends PassCore {
-  readonly altitude?: string;
-  readonly brightness?: string;
-  readonly description?: string;
-  readonly timeToPass?: string;
+    readonly altitude?: string;
+    readonly brightness?: string;
+    readonly description?: string;
+    readonly timeToPass?: string;
+    readonly direction?: string;
+    readonly compass?: string;
+    readonly azimuth?: { appear: number; disappear: number };
 }
 
 export type Pass = PassHome | PassMap;
